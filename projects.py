962d22408eb8145a6d1bab8120bc7754ecdc9dac
MSRAKHA <104523815+MSRAKHA@users.noreply.github.com>
2026-01-29 18:50:26 +0530

    Update projects.py

--- projects.py
+++ projects.py
@@ -232,7 +232,7 @@ def main():
              {
                  "name":"Databricks Certificate Badges Account",
                  "description": "https://www.credentials.net/profile/rakhashaik7/wallet",
-                 "url" :"https://www.credentials.net/profile/rakhashaik7/wallet",
+                 "url" :"https://www.credential.net/profile/rakhashaik7/wallet",
                  "status": "DataBricks Learning Completion Badges",
                  "tech_stack":"AI & Data Engineering"
              },
